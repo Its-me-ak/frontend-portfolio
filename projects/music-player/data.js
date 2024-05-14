faa0882b41fb02eabe7ee34c13c96a982e818fe9
Mohd Aquib <akibkhan9984@gmail.com>
2024-05-15 01:46:58 +0530

    first commit

---
+++ projects/music-player/data.js
@@ -0,0 +1,50 @@
+let songs = [
+    {
+        name : 'All Time Low',
+        path : 'musics/all-time-low.mp3',
+        artist : 'Alex Gaskarth',
+        cover : 'imgs/anime.jpg'
+    },
+
+    {
+        name : 'Time Back',
+        path : 'musics/time-back.mp3',
+        artist : 'LLEU',
+        cover : 'imgs/anime1.jpg'
+    },
+
+    {
+        name : 'Moonlight',
+        path : 'musics/moonlight.mp3',
+        artist : 'XXXTENTACION',
+        cover : 'imgs/anime2.jpg'
+    },
+
+    {
+        name : 'My Way - NEFFEX',
+        path : 'musics/my way.mp3',
+        artist : 'Bryce Savage',
+        cover : 'imgs/anime3.webp'
+    },
+
+    {
+        name : 'Blows',
+        path : 'musics/blows.mp3',
+        artist : 'Paul Velchev',
+        cover : 'imgs/anime4.jpg'
+    },
+
+    {
+        name : 'Space cadet',
+        path : 'musics/space-cadet.mp3',
+        artist : 'Gunna, Metro Boomin',
+        cover : 'imgs/anime5.jpg'
+    },
+
+    {
+        name : 'Overwhelmed (Remix)',
+        path : 'musics/overwhelmed.mp3',
+        artist : 'Ryan Mack',
+        cover : 'imgs/anime6.jpg'
+    }
+]
